4bfd3b39ab041a1b571b744869f11b5752abc50a
yakshith <yakshith027@gmail.com>
2023-06-08 01:32:34 +0530

    logo added

--- frontend/src/components/HomePage/Navbar.jsx
+++ frontend/src/components/HomePage/Navbar.jsx
@@ -1,7 +1,18 @@
+import { Flex, Image } from '@chakra-ui/react'
 import React from 'react'
 
 const Navbar = () => {
-  return <div>Navbar</div>
+  return (
+    <Flex p={'0.5rem'} boxShadow={'rgba(0, 0, 0, 0.24) 0px 3px 8px'}>
+      <Image
+        px={'0.7rem'}
+        py='0.3rem'
+        borderRadius={'md'}
+        src='https://i.ibb.co/TRDL2L6/Car-1-removebg-preview-removebg-preview.png'
+        width={'15rem'}
+      />
+    </Flex>
+  )
 }
 
 export default Navbar
